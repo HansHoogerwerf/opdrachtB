486da24b67b6da6f858cc7d748d82f68540640f1
Hans Hoogerwerf <0890158@hr.nl>
2015-12-12 18:09:34 +0100

    2nd build

---
+++ ReaderConverterAndDatabase/__init__test.py
@@ -0,0 +1,17 @@
+__author__ = 'Hans'
+
+import unittest
+from __init__ import splitConnectionsData
+
+class splitConnectionsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
