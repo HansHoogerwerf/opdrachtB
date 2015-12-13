ed89de013d99bb07478234cadeabef71c362b71d
Hans Hoogerwerf <0890158@hr.nl>
2015-12-13 02:38:37 +0100

    pls no monitoring

--- ReaderConverterAndDatabase/__init__.py
+++ ReaderConverterAndDatabase/__init__.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
 
         splitConnectionsData()
         splitEventsData()
-        splitMonitoringData()
+        #splitMonitoringData()
         #splitPositionsData()
 
 
